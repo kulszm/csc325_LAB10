6708d2e4add7b023e664da31af3465f30f9cc16f
kulsomzaraei <kulsomzaraei@macbook-air>
2022-11-11 21:48:32 -0500

    requirement 1: clear record

--- MVVMExample_Firebase_JavaFX/src/main/java/modelview/AccessFBView.java
+++ MVVMExample_Firebase_JavaFX/src/main/java/modelview/AccessFBView.java
@@ -8,6 +8,9 @@ import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.QueryDocumentSnapshot;
 import com.google.cloud.firestore.QuerySnapshot;
 import com.google.cloud.firestore.WriteResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.OidcProviderConfig.CreateRequest;
+import com.google.firebase.auth.UserRecord;
 import com.mycompany.mvvmexample.FirestoreContext;
 import com.mycompany.mvvmexample.FirestoreContext;
 import com.mycompany.mvvmexample.FirestoreContext;
@@ -30,8 +33,7 @@ import models.Person;
 
 public class AccessFBView {
 
- 
-     @FXML
+    @FXML
     private TextField nameField;
     @FXML
     private TextField majorField;
@@ -41,17 +43,19 @@ public class AccessFBView {
     private Button writeButton;
     @FXML
     private Button regRecord;
-    
-    private Button regRecord(ActionEvent event){
+
+    private void regRecord(ActionEvent event) {
         registerUserInFB();
+
     }
     @FXML
     private Button readButton;
     @FXML
     private TextArea outputField;
-     private boolean key;
+    private boolean key;
     private ObservableList<Person> listOfUsers = FXCollections.observableArrayList();
     private Person person;
+
     public ObservableList<Person> getListOfUsers() {
         return listOfUsers;
     }
@@ -69,11 +73,11 @@ public class AccessFBView {
         addData();
     }
 
-        @FXML
+    @FXML
     private void writeRecord(ActionEvent event) {
         readFirebase();
     }
-    
+
     public void addData() {
 
         DocumentReference docRef = App.fstore.collection("References").document(UUID.randomUUID().toString());
@@ -84,57 +88,58 @@ public class AccessFBView {
         data.put("Age", Integer.parseInt(ageField.getText()));
         //asynchronously write data
         ApiFuture<WriteResult> result = docRef.set(data);
+
+        nameField.clear();
+        majorField.clear();
+        ageField.clear();
+
     }
-    
-        public boolean readFirebase()
-         {
-             key = false;
+
+    public boolean readFirebase() {
+        key = false;
 
         //asynchronously retrieve all documents
-        ApiFuture<QuerySnapshot> future =  App.fstore.collection("References").get();
+        ApiFuture<QuerySnapshot> future = App.fstore.collection("References").get();
         // future.get() blocks on response
         List<QueryDocumentSnapshot> documents;
-        try 
-        {
+        try {
             documents = future.get().getDocuments();
-            if(documents.size()>0)
-            {
+            if (documents.size() > 0) {
                 System.out.println("Outing....");
-                for (QueryDocumentSnapshot document : documents) 
-                {
-                    outputField.setText(outputField.getText()+ document.getData().get("Name")+ " , Major: "+
-                            document.getData().get("Major")+ " , Age: "+
-                            document.getData().get("Age")+ " \n ");
+                for (QueryDocumentSnapshot document : documents) {
+                    outputField.setText(outputField.getText() + document.getData().get("Name") + " , Major: "
+                            + document.getData().get("Major") + " , Age: "
+                            + document.getData().get("Age") + " \n ");
                     System.out.println(document.getId() + " => " + document.getData().get("Name"));
-                    person  = new Person(String.valueOf(document.getData().get("Name")), 
+                    person = new Person(String.valueOf(document.getData().get("Name")),
                             document.getData().get("Major").toString(),
                             Integer.parseInt(document.getData().get("Age").toString()));
                     listOfUsers.add(person);
                 }
+            } else {
+                System.out.println("No data");
             }
-            else
-            {
-               System.out.println("No data"); 
-            }
-            key=true;
-            
-        }
-        catch (InterruptedException | ExecutionException ex) 
-        {
-             ex.printStackTrace();
+            key = true;
+
+        } catch (InterruptedException | ExecutionException ex) {
+            ex.printStackTrace();
         }
         return key;
     }
-       private void registerUserInFB(){
-            CreateRequest request = new CreateRequest()
-    .setEmail("user@example.com")
-    .setEmailVerified(false)
-    .setPassword("secretPassword")
-    .setPhoneNumber("+11234567890")
-    .setDisplayName("John Doe")
-    .setPhotoUrl("http://www.example.com/12345678/photo.png")
-    .setDisabled(false);
 
-UserRecord userRecord = FirebaseAuth.getInstance().createUser(request);
-System.out.println("Successfully created new user: " + userRecord.getUid());}
+    private void registerUserInFB() {
+        CreateRequest request;
+        request = new CreateRequest()
+                .setEmail("user@example.com")
+                .setEmailVerified(false)
+                .setPassword("secretPassword")
+                .setPhoneNumber("+11234567890")
+                .setDisplayName("John Doe")
+                .setPhotoUrl("http://www.example.com/12345678/photo.png")
+                .setDisabled(false);
+
+        UserRecord userRecord = FirebaseAuth.getInstance().createUser(request);
+        System.out.println("Successfully created new user: " + userRecord.getUid());
+
+    }
 }
